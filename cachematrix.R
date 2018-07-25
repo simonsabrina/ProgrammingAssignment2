e6298d7b8f7b5c1501fae8a4e59eaf797705d1db
Sabrina <simonssabrina@gmail.com>
2018-07-25 10:04:24 -0300

    R programming assignment 2
    Assignment 2 script for
    makeCacheMatrix( )
    cacheSolve( )

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Simulating what internet browsers do to avoid loading repeatedly the same
+## pages accessed by the user, we're going to build two functions that work
+## together in order to set the S3 objects and check for the existence of the
+## data (and get it) before load the page data.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function who creates an invertible matrix, as well sets
+## all the functions needed in its environment for the next step.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+                }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## The next step is to check for cached data before do the calculation.
+## If there is none, it calculates the inverse matrix.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
